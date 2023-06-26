c1fa5dce9880e8b43ab6d68fa62a015ac04ea9a1
MatthLB <matheushuck06@gmail.com>
2023-06-26 10:35:07 -0300

    comentario

--- estudojs/console.js
+++
@@ -1,9 +0,0 @@
-var idade = 75
-
-if(idade < 16){
-    console.log("menor de idade não vota")
-}else if(idade >= 67 || idade < 18){
-    console.log("Voto opcional")
-}else{
-    console.log("Voto obrigadorio")
-}
